8356d298faaaa36529b10d53608608402492e4fb
SimoneGasperini <simone.gasperini4@unibo.it>
2023-07-04 18:29:05 +0200

    Minor fix for Python script configurations

--- distributedQFT/write_random_statevector.py
+++ distributedQFT/write_random_statevector.py
@@ -1,15 +1,17 @@
 from qiskit.quantum_info import random_statevector
 
-# set configs
+
+# SETTINGS
+#########################################
 num_qubits = 3
-dims = 2**num_qubits
-filepath = './init_statevector.csv'
 seed = 42
+#########################################
 
 
 if __name__ == '__main__':
 
-    statevector = random_statevector(dims=dims, seed=seed)
+    statevector = random_statevector(dims=2**num_qubits, seed=seed)
+    filepath = './init_statevector.csv'
 
     with open(filepath, mode='w') as file:
         file.write('real, imag\n')
